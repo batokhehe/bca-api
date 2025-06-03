dae9ded35ef4884b582ea1e027f27389d84ca9e2
batokhehe <dhikanaufal@gmail.com>
2025-06-03 09:26:37 +0700

    fix: response format

--- middleware/logger.dart
+++ middleware/logger.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+
 import 'package:dart_frog/dart_frog.dart';
 import 'package:my_project/services/mongo_service.dart';
+import 'package:my_project/utils.dart';
 
 final mongo = MongoService.getInstance(
   'mongodb://logger:gtx.lgd@10.3.15.86:27017/logs?authSource=admin',
@@ -21,6 +23,9 @@ Middleware loggerMiddleware() {
       // Read and copy response body
       final bodyStr = await response.body();
 
+      final jsonRequestBody = tryParseJson(requestBody);
+      final jsonResponseBody = tryParseJson(bodyStr);
+
       await mongo.insertLog(
         level: 'INFO',
         message: 'Request to ${request.url.path}',
@@ -30,20 +35,34 @@ Middleware loggerMiddleware() {
           'path': request.url.path,
           'queryParameters': request.url.queryParameters,
           'requestHeaders': request.headers,
-          'requestBody': tryParseJson(requestBody),
+          'requestBody': jsonRequestBody,
           'statusCode': response.statusCode,
           'responseHeaders': response.headers,
-          'responseBody': tryParseJson(bodyStr),
+          'responseBody': jsonResponseBody,
           'duration_ms': stopwatch.elapsedMilliseconds,
         },
       );
       stopwatch.stop();
+
+      final String jsonString;
+
+      if (!request.url.path.contains('account_balance')) {
+        final jsonObject = <String, dynamic>{};
+        jsonObject['responseCode'] = jsonResponseBody['responseCode'];
+        jsonObject['responseMessage'] = jsonResponseBody['responseMessage'];
+        jsonObject['amount'] = jsonResponseBody['amount'];
+        jsonObject['transactionDate'] = getTimestamp();
+        jsonString = json.encode(jsonObject);
+      } else {
+        jsonString = bodyStr;
+      }
       // Rebuild the response so it can be returned
-      return Response(
+      final updatedResponse = Response(
         statusCode: response.statusCode,
         headers: response.headers,
-        body: bodyStr,
+        body: jsonString,
       );
+      return updatedResponse;
     };
   };
 }
