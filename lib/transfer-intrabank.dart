7a461eea278651b57ac941e59a6f8792d600892e
batokhehe <dhikanaufal@gmail.com>
2025-06-09 17:00:18 +0700

    feat: transfer intrabank

---
+++ lib/transfer-intrabank.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:my_project/const.dart';
+
+Future transferIntrabank(
+  String timestamp,
+  String xSignature,
+  String accessToken,
+  dynamic params,
+) async {
+  // External API URL
+  final url = Uri.parse(
+    API_TRANSFER_INTRABANK,
+  );
+
+  final requestHeader = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer $accessToken',
+    'X-TIMESTAMP': timestamp,
+    'X-SIGNATURE': xSignature,
+    'CHANNEL-ID': CHANNEL_ID,
+    'X-PARTNER-ID': PARTNER_ID,
+    'X-EXTERNAL-ID': params['partnerReferenceNo'].toString(),
+  };
+
+  final requestBody = jsonEncode(params);
+
+  // Send POST request
+  final response =
+      await http.post(url, headers: requestHeader, body: requestBody);
+
+  return jsonDecode(response.body);
+}
---
+++ routes/transfer_intrabank.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:my_project/access-token.dart';
+import 'package:my_project/const.dart';
+import 'package:my_project/generator/asymmetric-generator.dart';
+import 'package:my_project/generator/symmetric-generator.dart';
+import 'package:my_project/transfer-intrabank.dart';
+import 'package:my_project/utils.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  if (context.request.method != HttpMethod.post) {
+    return Response(statusCode: 405, body: 'Method Not Allowed');
+  }
+
+  final timestamp = getTimestamp();
+
+  const keyPath = 'certs/private_key.pem'; // Adjust if needed
+  final keyFile = File(keyPath);
+  if (!await keyFile.exists()) {
+    return Response.json(
+      statusCode: 500,
+      body: {'error': 'Private key file not found'},
+    );
+  }
+
+  final xAsymmetric = await generateAsymmetric(timestamp, keyFile);
+
+  try {
+    // Send POST request
+    final responseToken = await getAccessToken(timestamp, xAsymmetric);
+
+    final data = jsonDecode(responseToken);
+    final requestBody = jsonDecode(await context.request.body());
+    final accessToken = data['accessToken'].toString();
+
+    requestBody['partnerReferenceNo'] = getPartnerReferenceNo();
+    requestBody['transactionDate'] = timestamp;
+
+    final xSymmetric = await generateSymmetric(
+      HTTP_METHOD_POST,
+      accessToken,
+      requestBody,
+      timestamp,
+      CLIENT_SECRET,
+      ENDPOINT_TRANSFER_INTRABANK,
+    );
+
+    final response = await transferIntrabank(
+      timestamp,
+      xSymmetric,
+      accessToken,
+      requestBody,
+    );
+
+    return Response.json(body: response);
+  } catch (e) {
+    return Response.json(
+      statusCode: 500,
+      body: {'error': 'Exception: $e'},
+    );
+  }
+}
